5bece21d2adcb210a4b27029a22537b53dc8389c
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-15 00:04:56 +0200

    collections: add raw u8 storage and numeric swaps

--- src/cforge/collection_storage.clj
+++ src/cforge/collection_storage.clj
@@ -37,21 +37,31 @@
                 (reset! (get @stores right) a)
                 nil))}))
 
+(def ^:dynamic *u8-storage* (make-provider 0N))
 (def ^:dynamic *u64-storage* (make-provider 0N))
 (def ^:dynamic *usize-storage* (make-provider 0N))
 (def ^:dynamic *string-storage* (make-provider ""))
+
+(defn create-u8 [slots] ((:create *u8-storage*) slots))
+(defn slots-u8 [handle] ((:slots *u8-storage*) handle))
+(defn get-u8 [handle index] ((:get *u8-storage*) handle index))
+(defn set-u8! [handle index value] ((:set! *u8-storage*) handle index value))
+(defn resize-u8! [handle slots] ((:resize! *u8-storage*) handle slots))
+(defn swap-u8! [left right] ((:swap! *u8-storage*) left right))
 
 (defn create-u64 [slots] ((:create *u64-storage*) slots))
 (defn slots-u64 [handle] ((:slots *u64-storage*) handle))
 (defn get-u64 [handle index] ((:get *u64-storage*) handle index))
 (defn set-u64! [handle index value] ((:set! *u64-storage*) handle index value))
 (defn resize-u64! [handle slots] ((:resize! *u64-storage*) handle slots))
+(defn swap-u64! [left right] ((:swap! *u64-storage*) left right))
 
 (defn create-usize [slots] ((:create *usize-storage*) slots))
 (defn slots-usize [handle] ((:slots *usize-storage*) handle))
 (defn get-usize [handle index] ((:get *usize-storage*) handle index))
 (defn set-usize! [handle index value] ((:set! *usize-storage*) handle index value))
 (defn resize-usize! [handle slots] ((:resize! *usize-storage*) handle slots))
+(defn swap-usize! [left right] ((:swap! *usize-storage*) left right))
 
 (defn create-string [slots] ((:create *string-storage*) slots))
 (defn slots-string [handle] ((:slots *string-storage*) handle))
