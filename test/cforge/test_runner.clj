e612cf87b1b41c7266ac783c39690a3d9498df4a
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 22:01:23 +0200

    test: include CKV and hosted service suites

--- test/cforge/test_runner.clj
+++ test/cforge/test_runner.clj
@@ -2,8 +2,10 @@
   (:require [clojure.test :as t]
             [cforge.application-test]
             [cforge.arena-provider-test]
+            [cforge.ckv-test]
             [cforge.core-test]
             [cforge.freestanding-libraries-test]
+            [cforge.host-services-test]
             [cforge.library-contract-test]
             [cforge.module-link-test]
             [cforge.spec-coverage-test]))
@@ -11,8 +13,10 @@
 (defn -main [& _]
   (let [{:keys [fail error]} (t/run-tests 'cforge.application-test
                                           'cforge.arena-provider-test
+                                          'cforge.ckv-test
                                           'cforge.core-test
                                           'cforge.freestanding-libraries-test
+                                          'cforge.host-services-test
                                           'cforge.library-contract-test
                                           'cforge.module-link-test
                                           'cforge.spec-coverage-test)]
