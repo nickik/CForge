33bf41cb1795f93226bcd611fac60eff3cd6f826
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 15:32:32 +0200

    test: include Forge v1 feature coverage suite

--- test/cforge/test_runner.clj
+++ test/cforge/test_runner.clj
@@ -1,7 +1,9 @@
 (ns cforge.test-runner
   (:require [clojure.test :as t]
-            [cforge.core-test]))
+            [cforge.core-test]
+            [cforge.spec-coverage-test]))
 
 (defn -main [& _]
-  (let [{:keys [fail error]} (t/run-tests 'cforge.core-test)]
+  (let [{:keys [fail error]} (t/run-tests 'cforge.core-test
+                                          'cforge.spec-coverage-test)]
     (System/exit (if (zero? (+ fail error)) 0 1))))
