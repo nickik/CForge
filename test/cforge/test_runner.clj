563b8b2bf4cc3f81131a32d9a53b9c58f977fbe9
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 18:17:39 +0200

    Run core library contract tests

--- test/cforge/test_runner.clj
+++ test/cforge/test_runner.clj
@@ -1,9 +1,11 @@
 (ns cforge.test-runner
   (:require [clojure.test :as t]
             [cforge.core-test]
+            [cforge.library-contract-test]
             [cforge.spec-coverage-test]))
 
 (defn -main [& _]
   (let [{:keys [fail error]} (t/run-tests 'cforge.core-test
+                                          'cforge.library-contract-test
                                           'cforge.spec-coverage-test)]
     (System/exit (if (zero? (+ fail error)) 0 1))))
