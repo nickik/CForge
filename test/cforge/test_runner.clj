a27359e73aee6525676f782e62fabad907a42f63
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-14 18:53:13 +0200

    test: run Arena provider suite

--- test/cforge/test_runner.clj
+++ test/cforge/test_runner.clj
@@ -1,11 +1,13 @@
 (ns cforge.test-runner
   (:require [clojure.test :as t]
+            [cforge.arena-provider-test]
             [cforge.core-test]
             [cforge.library-contract-test]
             [cforge.spec-coverage-test]))
 
 (defn -main [& _]
-  (let [{:keys [fail error]} (t/run-tests 'cforge.core-test
+  (let [{:keys [fail error]} (t/run-tests 'cforge.arena-provider-test
+                                          'cforge.core-test
                                           'cforge.library-contract-test
                                           'cforge.spec-coverage-test)]
     (System/exit (if (zero? (+ fail error)) 0 1))))
