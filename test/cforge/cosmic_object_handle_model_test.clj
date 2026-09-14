2c5d978d6fb9344106f26106ccacc99c67746618
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-15 00:04:54 +0200

    Test validated ObjectId publication semantics

--- test/cforge/cosmic_object_handle_model_test.clj
+++ test/cforge/cosmic_object_handle_model_test.clj
@@ -15,11 +15,25 @@
         handles (oh/make-handle-table 2)
         object (:ok (oh/create-object! objects :memory-object {}))
         reservation (:ok (oh/reserve-slot! handles))
-        published (:ok (oh/publish! handles reservation (:id object)))]
+        published (:ok (oh/publish! handles objects reservation (:id object)))]
     (is (= {:ok (:id object)} (oh/lookup handles (:handle published))))
     (is (= 1 (oh/live-handle-count handles)))
     (is (= 0 (oh/reserved-handle-count handles)))
     (is (:ok (oh/mark-live! objects (:id object))))))
+
+(deftest publication-validates-object-identity-and-state
+  (let [objects (oh/make-object-table)
+        handles (oh/make-handle-table 2)
+        reservation-a (:ok (oh/reserve-slot! handles))]
+    (is (= :invalid-object
+           (:error (oh/publish! handles objects reservation-a 999))))
+    (is (= {:ok nil} (oh/cancel-reservation! handles reservation-a)))
+    (let [object (:ok (oh/create-object! objects :memory-object {}))
+          _ (oh/mark-live! objects (:id object))
+          reservation-b (:ok (oh/reserve-slot! handles))]
+      (is (= :invalid-object
+             (:error (oh/publish! handles objects reservation-b (:id object)))))
+      (is (= {:ok nil} (oh/cancel-reservation! handles reservation-b))))))
 
 (deftest cancellation-never-publishes
   (let [handles (oh/make-handle-table 1)
@@ -34,7 +48,7 @@
         handles (oh/make-handle-table 1)
         object (:ok (oh/create-object! objects :memory-object {}))
         reservation (:ok (oh/reserve-slot! handles))
-        handle (:handle (:ok (oh/publish! handles reservation (:id object))))]
+        handle (:handle (:ok (oh/publish! handles objects reservation (:id object))))]
     (is (= {:ok nil} (oh/unpublish! handles handle)))
     (is (= :invalid-handle (:error (oh/lookup handles handle))))
     (let [new-reservation (:ok (oh/reserve-slot! handles))]
@@ -45,5 +59,5 @@
         handles (oh/make-handle-table 1)
         object (:ok (oh/create-object! objects :memory-object {}))
         reservation (:ok (oh/reserve-slot! handles))]
-    (is (:ok (oh/publish! handles reservation (:id object))))
+    (is (:ok (oh/publish! handles objects reservation (:id object))))
     (is (= :table-full (:error (oh/reserve-slot! handles))))))
